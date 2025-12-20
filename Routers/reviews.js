45db32a8eb3d8ba6b4c4642b7fe558b6fc71d5ce
Roshan Bilsore <roshanbilsore@gmail.com>
2025-12-20 21:08:05 +0530

    structure the code in different file

---
+++ Routers/reviews.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const router = express.Router();
+
+const wrapAsync = require("../utils/AsyncWrap.js");
+const ExpressError = require("../utils/customError.js");
+
+const { reviewSchema } = require("../schema.js");
+const Listing = require("../models/listing.js");
+const Review = require("../models/review.js");
+
+// ---------------------------
+// Validate Reviews Middleware
+// ---------------------------
+const validateReview = (req, res, next) => {
+    let { error } = reviewSchema.validate(req.body);
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    }
+    next();
+};
+
+// ---------------------------
+// Create Review
+// POST /listing/:id/reviews
+// ---------------------------
+router.post("/listing/:id/reviews", validateReview, wrapAsync(async (req, res) => {
+    const { id } = req.params;
+    const listing = await Listing.findById(id);
+
+    if (!listing) throw new ExpressError(404, "Listing Not Found");
+
+    const newReview = new Review(req.body.review);
+
+    listing.reviews.push(newReview);
+
+    await newReview.save();
+    await listing.save();
+
+    res.redirect(`/listing/${listing._id}`);
+}));
+
+// ---------------------------
+// Delete Review
+// DELETE /listing/:id/reviews/:reviewId
+// ---------------------------
+router.delete("/listing/:id/reviews/:reviewId", wrapAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+
+    res.redirect(`/listing/${id}`);
+}));
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Listing = require("./models/listing.js");
-const review = require("./models/review.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
@@ -9,10 +7,13 @@ const ExpressError = require("./utils/customError.js");
 const wrapAsync = require("./utils/AsyncWrap.js");
 const { reviewSchema } = require("./schema.js");
 
+
+
 const app = express();
 
 // Import Routers
 const listingRouter = require("./Routers/listing.js");
+const reviewRouter = require("./Routers/reviews.js");
 
 // View Engine
 app.engine("ejs", ejsMate);
@@ -27,6 +28,8 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Use Listing Router
 app.use("/listing", listingRouter);
+app.use("/", reviewRouter);
+
 
 // Mongoose Connection
 main().then(() => console.log("mongoose is connected"))
@@ -36,47 +39,11 @@ async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
 }
 
-// Validate Reviews Middleware
-const validateReview = (req, res, next) => {
-    let { error } = reviewSchema.validate(req.body);
-    if (error) {
-        let errMsg = error.details.map((el) => el.message).join(",");
-        throw new ExpressError(400, errMsg);
-    }
-    next();
-};
 
 // Home Route
 app.get("/", (req, res) => {
     res.send("server is started");
 });
-
-// ---------------------------
-// Review Routes
-// ---------------------------
-
-// Create Review
-app.post("/listing/:id/reviews", validateReview, wrapAsync(async (req, res) => {
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new review(req.body.review);
-
-    listing.reviews.push(newReview);
-
-    await newReview.save();
-    await listing.save();
-
-    res.redirect(`/listing/${listing._id}`);
-}));
-
-// Delete Review
-app.delete("/listing/:id/reviews/:reviewId", wrapAsync(async (req, res) => {
-    let { id, reviewId } = req.params;
-
-    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-    await review.findByIdAndDelete(reviewId);
-
-    res.redirect(`/listing/${id}`);
-}));
 
 // 404 Handler
 app.use((req, res) => {
